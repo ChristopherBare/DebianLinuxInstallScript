4c6a3d16781b201e4058f850964e446b3c0c1d99
Christopher Bare <christopherbare@outlook.com>
2018-03-04 12:58:20 -0500

    Added discord functionality

--- install.sh
+++ install.sh
@@ -12,19 +12,30 @@ sudo apt dist-upgrade
 
 sudo apt install git cowsay fortune sysvbanner lolcat tree nano thunderbird wine autokey-gtk steam wget default-jdk default-jre gimp ubuntu-gnome-desktop gnome-tweak-tool geany
 
-#Install Atom 
+#Download Atom
 
 wget https://atom-installer.github.com/v1.24.0/atom-amd64.deb
 
-#Install Netbeans
+#Download Netbeans
 
 wget http://download.netbeans.org/netbeans/8.2/final/bundles/netbeans-8.2-linux.sh
 
+#Download Discord (needs testing)
+
+wget https://discordapp.com/api/download?platform=linux&format=deb
+
 sleep 5
+
+echo "Installing NetBeans"
 
 sh netbeans-8.2-linux.sh
 
 echo "Installing Atom"
 
-sudo dpkg -i atom*.deb 
+sudo dpkg -i atom*.deb
 
+echo "Installing Discord"
+
+sudo dpkg -i discord*.deb
+
+echo "Install finished, distro is ready for use!"
